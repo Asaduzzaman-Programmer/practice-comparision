5fb6fb33388c99814efe7b35fb577619fb450588
asad <asaduzzamanprogrammer@gmail.com>
2024-02-03 12:59:10 +0600

    second

--- comparision.js
+++ comparision.js
@@ -5,3 +5,15 @@
 // console.log(15 <=10);
 
 // console.log(15 >=10);
+
+// console.log(10 == 10.1);
+
+// console.log(10 == 10);
+
+// var applePrice = 40;
+// var peyaraPrice = 250;
+
+// console.log( applePrice < peyaraPrice);
+
+// console.log(10 == "10");
+// console.log(10 === "10");
