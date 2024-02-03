17c5e5118d9b370fe9bf569f6e8204ed08827073
asad <asaduzzamanprogrammer@gmail.com>
2024-02-03 13:18:53 +0600

    if

--- comparision.js
+++ comparision.js
@@ -1,19 +1,19 @@
-//  console.log(5 == 10);
+ console.log(5 == 10);
 
-// console.log(10 >= 10);
+console.log(10 >= 10);
 
-// console.log(15 <=10);
+console.log(15 <=10);
 
-// console.log(15 >=10);
+console.log(15 >=10);
 
-// console.log(10 == 10.1);
+console.log(10 == 10.1);
 
-// console.log(10 == 10);
+console.log(10 == 10);
 
-// var applePrice = 40;
-// var peyaraPrice = 250;
+var applePrice = 40;
+var peyaraPrice = 250;
 
-// console.log( applePrice < peyaraPrice);
+console.log( applePrice < peyaraPrice);
 
-// console.log(10 == "10");
-// console.log(10 === "10");
+console.log(10 == "10");
+console.log(10 === "10");
---
+++ if.js
@@ -0,0 +1,18 @@
+if (10 == 13){
+    console.log('I never ever go korea');
+}
+
+else{
+    console.log( "i will go china")
+}
+
+var biriyaniPrice = 550;
+
+if (biriyaniPrice < 500){
+    console.log("give me 25 packet")
+}
+
+else(
+    console.log("give me 10 packet")
+)
+    
