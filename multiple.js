ea09f51a7e1846fcdc84a2ed195d431a8f6ea410
asad <asaduzzamanprogrammer@gmail.com>
2024-02-04 13:17:17 +0600

    multiple

---
+++ multiple.js
@@ -0,0 +1,11 @@
+const isBsc = 'yes';
+const beton = 50000;
+const height = 96;
+
+if ( beton > 60000 || height > 90 && isBsc == 'yes'){
+    console.log("meyer ma raji")
+}
+
+else(
+    console.log('meyer ma raji na')
+)
