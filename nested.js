55407a82e7d97d77509930b650facccef80d16c0
asad <asaduzzamanprogrammer@gmail.com>
2024-02-04 14:01:46 +0600

    nested

---
+++ nested.js
@@ -0,0 +1,19 @@
+const money = 99;
+
+if (money >= 500){
+    console.log("taile tumi borolok")
+}
+else{
+    if(money >= 250){
+        console.log("tui gorib o na borolok o na")
+    }
+    else{
+        if(money >= 100){
+            console.log("mandarin tui ekta")
+        }
+        else{
+            console.log("apne okat me raha karo beukuf kahike")
+        }
+    }
+    
+}
