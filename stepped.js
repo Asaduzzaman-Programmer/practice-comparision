eefa1632dd9270782ab5c8732ce59f330b9c2495
asad <asaduzzamanprogrammer@gmail.com>
2024-02-04 13:25:04 +0600

    stepped

---
+++ stepped.js
@@ -0,0 +1,14 @@
+const price = 5000;
+
+if (price >= 5000){
+
+    // 10% discount 
+
+    const discount = price * 10/100;
+
+    // console.log(discountPrice);
+    
+    const discountPrice = price - discount;
+    console.log(discountPrice);
+
+}
